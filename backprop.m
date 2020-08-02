da2ec2d6affaf8691f659bec97fe3d0244dff896
Enes AKGUN <60859449+eakgun@users.noreply.github.com>
2020-08-02 22:18:13 +0300

    Update backprop.m

--- backprop.m
+++ backprop.m
@@ -1,23 +1,37 @@
-function output = backprop(o, K, Whh, Who,zt_saved,ht_saved)
+function [dWih, dWhh, dWho,del_t] = backprop(Wih, Whh, Who, ht, zt, o, train_out, train_in, K)
     
     e_t = 2 * (o - train_out(1:K));
-    del_t = [];
-
-
+    del_t = []; %columns are t backwards through time.
+    ind = 0;
+    dWih = 0;
+    dWhh = 0; 
+    dWho = 0;
     t = K;  
-    while t > 1
+    while t > 0
        
 
         if t == K 
-           del_t = [del_t, Who'* et(t) .* dtanh(zt_saved(:,t))];
-           del_next = del_t;
+            del_t = [del_t, Who'* e_t(t) .* dtanh(zt(:,t))];
+            
         else
-            del_t = (Who'*e(t)+Whh'*del_next) .* dtanh(zt_saved(:,t));
-            del_next = del_t;
+            del_t = [del_t, (Who'*e_t(t)+Whh'*del_t(:,ind)) .* dtanh(zt(:,t))];
+           
         end
+        ind = ind + 1;
         t = t - 1;
 
     end
+    
+    del_rev = fliplr(del_t);
 
-
+    for i = 1:K
+        
+        dWho = dWho + e_t(i)*ht(:,i+1);
+        dWih = dWho + del_rev(:,i)*train_in(i);
+        dWhh = dWhh + del_rev(:,i);
+    end
+     
+    
 end
+
+
